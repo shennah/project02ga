235f34734b19da1d9d3248e6342d1b8a56106bc7
Shennah <shennah.a@hotmail.com>
2015-11-24 15:40:29 +1100

    made a thing

--- app/assets/javascripts/tinderhustler.js
+++ app/assets/javascripts/tinderhustler.js
@@ -30,3 +30,96 @@ $(document).ready(function(){
 	console.log('test before insert')
 
 });
+
+
+//// SHENNAHS STUFF
+
+var map;
+var marker;
+var markers = [];
+var latitude;
+var longitude;
+
+
+
+function initMap() {
+
+  // initialise map, sets starting lat/lng, zoom
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  });
+
+
+  // map click event listener
+  google.maps.event.addListener(map, 'click', function(event) {
+    placeMarker(event.latLng); // calls place marker function
+    latitude  = event.latLng.lat(); // stores lat and lng var on click
+    longitude = event.latLng.lng();
+    console.log("Latitude " + latitude + ", Longitude " + longitude)
+
+  });
+
+
+  // undo button
+  $('.undo').on('click', function(){
+    markers.pop(marker);
+    marker.setMap(null);
+  });
+
+
+
+} // initMap
+
+
+function placeMarker(location) {
+  // places marker - sets options
+  if (marker) {
+    marker.setMap(null);
+  }
+
+  marker = new google.maps.Marker({
+      animation: google.maps.Animation.DROP,
+      position: location, 
+      map: map,
+      draggable: true,
+  });
+  markers.push(marker);
+
+
+  // adds info box with content to dropped marker 
+  var infowindow = new google.maps.InfoWindow({
+    content: "wut u lookin at m8"
+  });
+
+  // open info on double click
+  marker.addListener('dblclick', function() {
+    infowindow.open(map, this, marker);
+  });
+
+  google.maps.event.addListener(marker, 'dragend', function(e){
+      // document.getElementById('testing').innerHTML = '<p>Marker dropped: Current Lat: ' + e.latLng.lat().toFixed(3) + ' Current Lng: ' + e.latLng.lng().toFixed(3) + '</p>';
+    latitude  = e.latLng.lat(); // stores lat and lng var on click
+    longitude = e.latLng.lng();
+    console.log("New Latitude " + latitude + ", New Longitude " + longitude)
+  });
+
+  // google.maps.event.addListener(marker, 'dragstart', function(e){
+  //     document.getElementById('testing').innerHTML = '<p>Currently dragging marker...</p>';
+  // });
+
+}
+
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+function deleteMarkers() {
+  setMapOnAll(null);
+  markers = [];
+}
+
+
